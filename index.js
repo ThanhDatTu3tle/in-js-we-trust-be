365643fd5979e4ecd94c0802c43947a3cc5bac6b
ThanhDatTu3tle <thanhdat11badinh@gmail.com>
2023-02-13 21:34:35 +0700

    update something to deloy

--- index.js
+++ index.js
@@ -10,7 +10,9 @@ import question from './routers/question.route.js';
 
 const app = express()
 const PORT = process.env.port || 5000
-const URI = 'mongodb+srv://tu3tle:dat27032001@cluster0.tmunkuo.mongodb.net/?retryWrites=true&w=majority'
+// const URI = process.env.MONGO_URI.toString()
+const URI = 'mongodb+srv://tu3tle:dat27032001@cluster0.tmunkuo.mongodb.net/'
+
 // use middleware
 app.use(bodyParser.json({ limit: '30mb' }))
 app.use(bodyParser.urlencoded({ extended: true, limit: '30mb' }))
